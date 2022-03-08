9f47cafda4df987fe41a8696df00f40e3455306e
Alex <alex.perez@ufl.edu>
2022-03-08 18:56:35 -0500

    now linting issues are fixed in darknet

--- src/connected_layer.h
+++ src/connected_layer.h
@@ -10,7 +10,8 @@ typedef layer connected_layer;
 #ifdef __cplusplus
 extern "C" {
 #endif
-connected_layer make_connected_layer(int batch, int steps, int inputs, int outputs, ACTIVATION activation, int batch_normalize);
+connected_layer make_connected_layer(int batch, int steps, int inputs, int outputs, ACTIVATION activation,
+                                     int batch_normalize);
 size_t get_connected_workspace_size(layer l);
 
 void forward_connected_layer(connected_layer layer, network_state state);
@@ -22,7 +23,8 @@ void statistics_connected_layer(layer l);
 #ifdef GPU
 void forward_connected_layer_gpu(connected_layer layer, network_state state);
 void backward_connected_layer_gpu(connected_layer layer, network_state state);
-void update_connected_layer_gpu(connected_layer layer, int batch, float learning_rate, float momentum, float decay, float loss_scale);
+void update_connected_layer_gpu(connected_layer layer, int batch, float learning_rate, float momentum, float decay,
+                                float loss_scale);
 void push_connected_layer(connected_layer layer);
 void pull_connected_layer(connected_layer layer);
 #endif
