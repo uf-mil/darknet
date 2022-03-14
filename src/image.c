a29098e125ff7b7ab5cad4d5de6d71a52a5476a1
Alex Perez <alex.perez@ufl.edu>
2022-03-13 20:38:21 -0400

    removed print statement

--- src/image.c
+++ src/image.c
@@ -411,15 +411,13 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
     {
       if (selected_detections[i].det.prob[j] > thresh && j != best_class)
       {
-        printf("%s: %.0f%%", names[j], selected_detections[i].det.prob[j] * 100);
+        // printf("%s: %.0f%%", names[j], selected_detections[i].det.prob[j] * 100);
 
         if (ext_output)
           printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
                  round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2) * im.w),
                  round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2) * im.h),
                  round(selected_detections[i].det.bbox.w * im.w), round(selected_detections[i].det.bbox.h * im.h));
-        else
-          printf("\n");
       }
     }
   }
@@ -587,7 +585,7 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
         top = 0;
       if (bot > im.h - 1)
         bot = im.h - 1;
-      printf("%s: %.0f%%", names[class_id], prob * 100);
+      // printf("%s: %.0f%%", names[class_id], prob * 100);
 
       // printf(" - id: %d, x_center: %d, y_center: %d, width: %d, height: %d",
       //    class_id, (right + left) / 2, (bot - top) / 2, right - left, bot - top);
